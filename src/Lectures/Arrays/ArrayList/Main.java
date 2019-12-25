12acf327d5ea19d44846f0094d3b3a3ab33434e1
Tammam ABOU RSHAID <43439577+tammamabourshaid@users.noreply.github.com>
2019-12-25 14:47:28 +0100

    Udemy_Java

--- src/Lectures/Arrays/ArrayList/Main.java
+++ src/Lectures/Arrays/ArrayList/Main.java
@@ -173,7 +173,7 @@ public class Main {
         ArrayList<String> newArray = new ArrayList<String>();
         newArray.addAll(groceryList.getGroceryList());
 
-        ArrayList<String> nextArray = new ArrayList<String>(groceryList.getGroceryList());
+        ArrayList<String> nextArray = new ArrayList<String>(groceryList.getGroceryList()); //Copy the content of the Array to a NEW Array
 
         String[] myArray = new String[groceryList.getGroceryList().size()];
         myArray = groceryList.getGroceryList().toArray(myArray);
