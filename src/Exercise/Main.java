36fae9c3d91ea1c668dc0d415928e42772e7b9be
Tammam ABOU RSHAID <43439577+tammamabourshaid@users.noreply.github.com>
2019-12-25 23:19:45 +0100

    Udemy_Java

--- src/Exercise/Main.java
+++ src/Exercise/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " scored " + score + " points");
         return score * 1000;
+
+
     }
 
     
