43b264c57c0987e4141508deca25081205fcaa18
Tammam ABOU RSHAID <43439577+tammamabourshaid@users.noreply.github.com>
2019-12-28 23:56:11 +0100

    Udemy_Java

--- src/Lectures/Abstract/Challenge/Node.java
+++ src/Lectures/Abstract/Challenge/Node.java
@@ -33,7 +33,7 @@ public class Node extends ListItem {
 
     @Override
     int compareTo(ListItem item) {
-        if(item != null) {
+        if (item != null) {
             return ((String) super.getValue()).compareTo((String) item.getValue());
         } else {
             return -1;
---
+++ src/Lectures/Generic/Challenge/FootballPlayer.java
@@ -0,0 +1,11 @@
+package Lectures.Generic.Challenge;
+
+/**
+ * Created by dev on 17/10/2015.
+ */
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
